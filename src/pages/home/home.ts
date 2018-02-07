c6d0c2d9253a88b527ea482554d3086e01fb4950
Renato Filho <renatosousafilho@gmail.com>
2018-02-07 10:45:36 -0300

    chamando função via js

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -14,8 +14,38 @@ export class HomePage {
     this.platform.ready().then(() => {
       let browser = new InAppBrowser("https://demo.lazarus.bet",'_blank', 'location=no');
       browser.on("loadstop").subscribe((event) => {
-        browser.executeScript({ code: "$('#sidebar-right-button').click(function(){alert('teste');});" });
+        browser.executeScript({ code: "window.localStorage.setItem('name', '')"});
+        browser.executeScript({ code:
+          "$('#sidebar-right-button').click(function(){window.localStorage.setItem('name','teste')})"
+        }).then( data => {
+          console.log('loadstop');
+        });
+        this.setName(browser);
+
+
       });
     });
+  }
+
+  public setName(browser) {
+    var $self = this;
+    var loop = setInterval(function() {
+      browser.executeScript({
+        code: "localStorage.getItem( 'name' )"
+      }).then(function( values ) {
+        var name = values[ 0 ];
+        if ( name ) {
+          alert(name);
+          browser.executeScript({ code: "window.localStorage.setItem('name', '')"});
+          clearInterval( loop );
+          $self.setName(browser);
+        }
+      });
+    });
+
+  }
+
+  public teste() {
+    console.log("DEBUG HERE");
   }
 }
